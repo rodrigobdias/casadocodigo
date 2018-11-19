4522814f71bfb4748953eca261fd0d98473b37af
Rodrigo <rodrigobdias@gmail.com>
2018-11-19 18:55:26 -0200

    Ajustando a home.jsp

--- src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/HomeController.java
@@ -1,14 +1,28 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.casadocodigo.loja.daos.ProdutoDAO;
+import br.com.casadocodigo.loja.models.Produto;
 
 @Controller
 public class HomeController {
+	
+    @Autowired
+    ProdutoDAO produtoDao;
 
-	@RequestMapping("/")
-	private String index() {
-		System.out.println("Entrando no Home da CasaDoCodigo");
-		return "home";
-	}
+    @RequestMapping("/")
+    public ModelAndView index(){
+        List<Produto> produtos = produtoDao.listar();
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("produtos", produtos);
+		System.out.println("Entrando no Home da CasaDoCodigo (CDC)");
+        return modelAndView;
+    }
+    
 }
