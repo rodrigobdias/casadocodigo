1e529ea191a63b2d8c5fe554dc7297a179ab466a
Rodrigo <rodrigobdias@gmail.com>
2018-11-23 12:07:44 -0200

    Spring Security: Autorizando Acesso

--- src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/SecurityConfiguration.java
@@ -1,9 +1,25 @@
 package br.com.casadocodigo.loja.conf;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.authorizeRequests()
+	    .antMatchers("/resources/**").permitAll()
+	    .antMatchers("/carrinho/**").permitAll()
+	    .antMatchers("/pagamento/**").permitAll()
+	    .antMatchers("/produtos/form").hasRole("ADMIN")
+	    .antMatchers(HttpMethod.POST, "/produtos").hasRole("ADMIN")
+	    .antMatchers(HttpMethod.GET, "/produtos").hasRole("ADMIN")
+	    .antMatchers("/produtos/**").permitAll()
+	    .antMatchers("/").permitAll()
+	    .anyRequest().authenticated()
+	    .and().formLogin();
+	}
 }
