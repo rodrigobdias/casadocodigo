c3e84bb819dce357f2543cb2ad7dd9ff9c58f0bf
Rodrigo <rodrigobdias@gmail.com>
2018-12-04 15:29:01 -0200

    Testando a aplicação: Trabalhando com profiles

--- src/test/java/br/com/casadocodigo/loja/daos/ProdutoDAOTest.java
+++ src/test/java/br/com/casadocodigo/loja/daos/ProdutoDAOTest.java
@@ -9,16 +9,19 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.com.casadocodigo.loja.builders.ProdutoBuilder;
 import br.com.casadocodigo.loja.conf.JPAConfiguration;
+import br.com.casadocodigo.loja.confs.DataSourceConfigurationTest;
 import br.com.casadocodigo.loja.models.Produto;
 import br.com.casadocodigo.loja.models.TipoPreco;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {JPAConfiguration.class,ProdutoDAO.class})
+@ContextConfiguration(classes = {JPAConfiguration.class, ProdutoDAO.class, DataSourceConfigurationTest.class})
+@ActiveProfiles("test")
 public class ProdutoDAOTest {
 
     @Autowired
