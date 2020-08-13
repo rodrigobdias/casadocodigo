2f190842ad6d5a555caab1e03b7400de60c6ab69
rodrigobdias <43188140+rodrigobdias@users.noreply.github.com>
2020-08-12 22:40:18 -0300

    Update AppWebConfiguration.java

--- src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
+++ src/main/java/br/com/casadocodigo/loja/conf/AppWebConfiguration.java
@@ -57,6 +57,7 @@ public class AppWebConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
+		
 		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
