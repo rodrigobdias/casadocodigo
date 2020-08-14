d7f61c8e2ebb610ea0aced17c0ec68156bb77448
rodrigobdias <43188140+rodrigobdias@users.noreply.github.com>
2020-08-13 22:17:31 -0300

    Update Preco.java

--- src/main/java/br/com/casadocodigo/loja/models/Preco.java
+++ src/main/java/br/com/casadocodigo/loja/models/Preco.java
@@ -13,12 +13,15 @@ public class Preco {
 	public BigDecimal getValor() {
 		return valor;
 	}
+	
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
+	
 	public TipoPreco getTipo() {
 		return tipo;
 	}
+	
 	public void setTipo(TipoPreco tipo) {
 		this.tipo = tipo;
 	}
