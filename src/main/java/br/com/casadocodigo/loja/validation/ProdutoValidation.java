6590a27e73b80774f234d9563e5b0cda93907e0d
rodrigobdias <43188140+rodrigobdias@users.noreply.github.com>
2020-08-13 22:17:51 -0300

    Update ProdutoValidation.java

--- src/main/java/br/com/casadocodigo/loja/validation/ProdutoValidation.java
+++ src/main/java/br/com/casadocodigo/loja/validation/ProdutoValidation.java
@@ -10,11 +10,13 @@ public class ProdutoValidation implements Validator{
 	
 	@Override
 	public boolean supports(Class<?> clazz) {
+		
 		return Produto.class.isAssignableFrom(clazz);
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {	
+		
 		ValidationUtils.rejectIfEmpty(errors, "titulo", "field.required");
 		ValidationUtils.rejectIfEmpty(errors, "descricao", "field.required");
 		
